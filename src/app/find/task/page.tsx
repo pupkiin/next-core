f62ad53419d31b0d6a13b55b3625293e2d2ed322
Danila <danila.pupyshev@gmail.com>
2023-12-16 21:19:00 +0300

    edit find/task page

--- src/app/find/task/page.tsx
+++ src/app/find/task/page.tsx
@@ -1,11 +1,153 @@
-
-
+import Development from "@/components/ui/FindMasterTypes/Development/Develompent";
+import styles from "./findTask.module.css";
+ 
 export default function findTask() {
   return (
-    <main>
-      <div>
-        <h1>find task</h1>
-        <div>fskfmk</div>
+    <main className={styles.container}>
+      <div className={styles.findTaskTitleBlock}>
+        <h1 className={styles.findTaskTitle}>
+          <svg
+            className={styles.findTaskTitleImg}
+            fill="currentColor"
+            viewBox="0 0 16 16"
+            height="1em"
+            width="1em"
+          >
+            <path
+              fillRule="evenodd"
+              d="M1.5 1.5A.5.5 0 001 2v4.8a2.5 2.5 0 002.5 2.5h9.793l-3.347 3.346a.5.5 0 00.708.708l4.2-4.2a.5.5 0 000-.708l-4-4a.5.5 0 00-.708.708L13.293 8.3H3.5A1.5 1.5 0 012 6.8V2a.5.5 0 00-.5-.5z"
+            />
+          </svg>
+          Найди работу для себя
+        </h1>
+        <p className={styles.findTaskDescription}>
+          Ищите работу, выполняйте поставленную клиентом задачу и получайте
+          деньги.
+        </p>
+
+        <div className={styles.findTaskExploreBlock}>
+          <div className={styles.findTaskTypes}>
+            <h2 className={styles.findTaskExploreBlockTitle}>
+              Выберите направление
+            </h2>
+
+            <div className={styles.radioControl}>
+              <label className={styles.radioLabel}>
+                <input
+                  type="radio"
+                  name="1"
+                  className="radio focus:outline-3 focus:outline-indigo-300 checked:bg-indigo-500"
+                  defaultChecked
+                />
+                <span className={styles.radioType}>Разработка и IT</span>
+              </label>
+            </div>
+
+            <div className={styles.radioControl}>
+              <label className={styles.radioLabel}>
+                <input
+                  type="radio"
+                  name="1"
+                  className="radio focus:outline-3 focus:outline-indigo-300 checked:bg-indigo-500"
+                />
+                <span className={styles.radioType}>Дизайн и творчество</span>
+              </label>
+            </div>
+
+            <div className={styles.radioControl}>
+              <label className={styles.radioLabel}>
+                <input
+                  type="radio"
+                  name="1"
+                  className="radio focus:outline-3 focus:outline-indigo-300 checked:bg-indigo-500"
+                />
+                <span className={styles.radioType}>Продажи и маркетинг</span>
+              </label>
+            </div>
+
+            <div className={styles.radioControl}>
+              <label className={styles.radioLabel}>
+                <input
+                  type="radio"
+                  name="1"
+                  className="radio focus:outline-3 focus:outline-indigo-300 checked:bg-indigo-500"
+                />
+                <span className={styles.radioType}>
+                  Администрирование и поддежка клиентов
+                </span>
+              </label>
+            </div>
+
+            <div className={styles.radioControl}>
+              <label className={styles.radioLabel}>
+                <input
+                  type="radio"
+                  name="1"
+                  className="radio focus:outline-3 focus:outline-indigo-300 checked:bg-indigo-500"
+                />
+                <span className={styles.radioType}>Тексты и переводы</span>
+              </label>
+            </div>
+
+            <div className={styles.radioControl}>
+              <label className={styles.radioLabel}>
+                <input
+                  type="radio"
+                  name="1"
+                  className="radio focus:outline-3 focus:outline-indigo-300 checked:bg-indigo-500"
+                />
+                <span className={styles.radioType}>Финансы и учёт</span>
+              </label>
+            </div>
+
+            <div className={styles.radioControl}>
+              <label className={styles.radioLabel}>
+                <input
+                  type="radio"
+                  name="1"
+                  className="radio focus:outline-3 focus:outline-indigo-300 checked:bg-indigo-500"
+                />
+                <span className={styles.radioType}>HR и обучение</span>
+              </label>
+            </div>
+
+            <div className={styles.radioControl}>
+              <label className={styles.radioLabel}>
+                <input
+                  type="radio"
+                  name="1"
+                  className="radio focus:outline-3 focus:outline-indigo-300 checked:bg-indigo-500"
+                />
+                <span className={styles.radioType}>
+                  Архетектура и инженерия
+                </span>
+              </label>
+            </div>
+
+            <div className={styles.radioControl}>
+              <label className={styles.radioLabel}>
+                <input
+                  type="radio"
+                  name="1"
+                  className="radio focus:outline-3 focus:outline-indigo-300 checked:bg-indigo-500"
+                />
+                <span className={styles.radioType}>
+                  Образование
+                </span>
+              </label>
+            </div>
+
+
+          </div>
+          <div className={styles.findTaskTaskTopics}>
+            <h2 className={styles.findTaskExploreBlockTitle}>
+              Найдите интересующую специальность
+            </h2>
+            
+            <Development />
+
+          </div>
+        </div>
       </div>
     </main>
   );
---
+++ src/components/ui/FindMasterTypes/Development/Develompent.tsx
@@ -0,0 +1,197 @@
+import Link from 'next/link';
+import styles from './Development.module.css'
+
+export default function Development() {
+  return (
+    <div className={styles.developmentBlock}>
+      <ul className={styles.developmentList}>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            IOS Разработчик
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Android Разработчик
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Разработчик API
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Разработчик ботов
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Разработчик Игр
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            C++ Разработчик
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            CRM специалист
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Инженер компьютерного зрения и искуственного интелекта
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            C# Разработчик
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Разработчик контроллеров
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Специалист по глубокому обучению
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Django Разработчик
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Аналитик Данных
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Figma специалист
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Front-End Разработчик
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Back-End Разрабочик
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Верстальщик
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Flutter Разработчик
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Google Разработчик
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Java Script Разработчик
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Linux Разработчик
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Мобильная разработка
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            MS Office 365 Специалист
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Excel Специалист
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Разработчик баз данных
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Python Разработчик
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            PHP Разработчик
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Разработчик Консольных приложений
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            React Разработчик
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            React Native Разработчик
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            SSL Специалист
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Unity Разработчик
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Vue Разработчик
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Angular Разработчик
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            WordPress Разработчик
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Web Дизайнер
+          </Link>
+        </li>
+        <li className={styles.developmentListItem}>
+          <Link className={styles.developmentItemLink} href="/tasks">
+            Java Разработчик
+          </Link>
+        </li>
+        
+      </ul>
+    </div>
+  ); 
+}
